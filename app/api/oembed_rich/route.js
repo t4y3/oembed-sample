41df70b9758c1d14bfb63a243115e8176d195f87
t4y3 <y.teraoka8@gmail.com>
2024-06-13 22:02:16 +0900

    fix

--- app/api/oembed_rich/route.js
+++ app/api/oembed_rich/route.js
@@ -4,14 +4,14 @@
 export async function GET(request) {
     return new Response({
             "author_name": "t4y3",
-            "html": "\u003Cdiv\u003Eくまさん\u003C/div\u003E",
+            "html": "\u003Cdiv\u003EKUMA_SAN\u003C/div\u003E",
             "width": 300,
             "height": 300,
             "version": "1.0",
             "provider_name": "oEmbed Sample",
             "provider_url": "https://oembed-sample.vercel.app/",
             "type": "rich",
-            "title": "Today’s Top Hits",
+            "title": "KUMA_SAN",
             "thumbnail_url": "https://i.scdn.co/image/ab67706f00000002100e87512faecef9861e5102",
             "thumbnail_width": 300,
             "thumbnail_height": 300
@@ -19,6 +19,7 @@ export async function GET(request) {
         }, {
             status: 200,
             headers: {
+                'Content-Type': 'application/json',
                 'Access-Control-Allow-Origin': '*',
                 'Access-Control-Allow-Methods': 'GET',
                 'Access-Control-Allow-Headers': 'Content-Type, Authorization',
