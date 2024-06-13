87d7ca4a3932c0c8be0ad1130c4e94fb2406e159
t4y3 <y.teraoka8@gmail.com>
2024-06-13 22:37:38 +0900

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: "/api/:path*",
+                headers: [
+                    {
+                        key: "Access-Control-Allow-Origin",
+                        value: "*", // Set your origin
+                    },
+                    {
+                        key: "Access-Control-Allow-Methods",
+                        value: "GET",
+                    },
+                ],
+            },
+        ];
+    },
+};
 
 export default nextConfig;
