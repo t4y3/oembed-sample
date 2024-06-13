e47132cb5e298d455f0908f504588ee334e94570
t4y3 <y.teraoka8@gmail.com>
2024-06-13 18:39:22 +0900

    fix

--- app/news/[newsId]/page.jsx
+++ app/news/[newsId]/page.jsx
@@ -5,7 +5,7 @@ export async function generateMetadata(
     return {
         alternates: {
             types: {
-                "'application/json+oembed'": `https://oembed-sample.vercel.app/api/oembed?newsId=${params.newsId}`
+                "'application/json+oembed'": `https://oembed-sample.vercel.app/api/oembed?url=https://oembed-sample.vercel.app/news/${params.newsId}`
             }
         }
     }
