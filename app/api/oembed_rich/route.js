0a16e546bef0aeecb6b171db4dbdbdefbe375df1
t4y3 <y.teraoka8@gmail.com>
2024-06-13 23:07:13 +0900

    fix

--- app/api/oembed_rich/route.js
+++ app/api/oembed_rich/route.js
@@ -7,7 +7,7 @@ export async function GET(request) {
     return NextResponse.json({
         "version": "1.0",
         "author_name": "t4y3",
-        "html": "\u003Cblockquote\u003Etest\u003C/blockquote\u003E",
+        "html": "\u003Cp\u003Etest\u003C/p\u003E",
         "width": 300,
         "height": 300,
         "provider_name": "oEmbed Sample",
