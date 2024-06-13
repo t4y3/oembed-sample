fd5c4e20ae0b8669d323ea3f8d1d9ee41dc8094f
t4y3 <y.teraoka8@gmail.com>
2024-06-13 21:01:51 +0900

    fix

--- app/api/oembed/route.js
+++ app/api/oembed/route.js
@@ -6,13 +6,19 @@ import {NextResponse} from 'next/server'
 export async function GET(request) {
 
     return NextResponse.json({
-            "type": "rich",
-            "version": "1.0",
-            "title": "埋め込みどうでしょう",
             "author_name": "t4y3",
-            "html": "\u003Cdiv style=\"width:100%;height:120px;border:1px solid;font-weight:bold;overflow:hidden;background-color:white;\"\u003E\u003Cimg alt=\"\" src=\"http://farm4.static.flickr.com/3123/2341623661_7c99f48bbf_m.jpg\" width=120 height=120 style=\"float:left;margin-right:10px;\"/\u003Eくまさん\u003C/div\u003E",
-            "height": 500,
-            "width": 500,
+            "html": "\u003Cdiv style=\"width:300px;height:300px;border:1px solid;font-weight:bold;overflow:hidden;background-color:white;\"\u003E\u003Cimg alt=\"\" src=\"http://farm4.static.flickr.com/3123/2341623661_7c99f48bbf_m.jpg\" width=120 height=120 style=\"float:left;margin-right:10px;\"/\u003Eくまさん\u003C/div\u003E",
+            "width": 300,
+            "height": 300,
+            "version": "1.0",
+            "provider_name": "Spotify",
+            "provider_url": "https://spotify.com",
+            "type": "rich",
+            "title": "Today’s Top Hits",
+            "thumbnail_url": "https://i.scdn.co/image/ab67706f00000002100e87512faecef9861e5102",
+            "thumbnail_width": 300,
+            "thumbnail_height": 300
+
         }
     )
 
