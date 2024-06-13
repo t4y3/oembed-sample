eab98208dca0044b433ed6526acc11b01d26e2c7
t4y3 <y.teraoka8@gmail.com>
2024-06-13 20:36:38 +0900

    fix

--- app/api/oembed/route.js
+++ app/api/oembed/route.js
@@ -7,7 +7,7 @@ export async function GET(request) {
             "version": "1.0",
             "title": "埋め込みどうでしょう",
             "author_name": "t4y3",
-            "html": "<div style=\"width:100%;height:120px;border:1px solid;font-weight:bold;overflow:hidden;background-color:white;\"><img src=\"https://t4y3.github.io/oembed-sample/img/Bears.jpg\" width=120 height=120 style=\"float:left;margin-right:10px;\"/>くまさん</div>",
+            "html": "<div style=\"width:100%;height:120px;border:1px solid;font-weight:bold;overflow:hidden;background-color:white;\"><img alt=\"\" src=\"http://farm4.static.flickr.com/3123/2341623661_7c99f48bbf_m.jpg\" width=120 height=120 style=\"float:left;margin-right:10px;\"/>くまさん</div>",
             "height": 500,
             "width": 500,
         }
