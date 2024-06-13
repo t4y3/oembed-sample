d6f885902bff920e96802e6bb1830775eac15f86
t4y3 <y.teraoka8@gmail.com>
2024-06-13 22:42:24 +0900

    fix

--- app/api/oembed_rich/route.js
+++ app/api/oembed_rich/route.js
@@ -7,12 +7,12 @@ export async function GET(request) {
     return NextResponse.json({
         "version": "1.0",
         "author_name": "t4y3",
-        "html": "\u003Ciframe src=\"https://oembed-sample.vercel.app\" frameborder=\"0\" width=\"300\" height=\"300\" allowfullscreen\u003E\u003C/iframe\u003E",
+        "html": "\u003Cdiv\u003Etest\u003C/div\u003E",
         "width": 300,
         "height": 300,
         "provider_name": "oEmbed Sample",
         "provider_url": "https://oembed-sample.vercel.app/",
         "type": "rich",
-        "title": "KUMA_SAN"
+        "title": "embed test"
     }, { status: 200 });
 }
