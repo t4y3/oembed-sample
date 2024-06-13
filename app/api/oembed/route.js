01daf9161a301337b442a7691794d6b1355555aa
t4y3 <y.teraoka8@gmail.com>
2024-06-13 18:55:55 +0900

    fix

--- app/api/oembed/route.js
+++ app/api/oembed/route.js
@@ -2,21 +2,27 @@ import {NextResponse} from 'next/server'
 
 export async function GET(request) {
 
-    console.log("\x1b[31m");
-    console.log("_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/");
-    console.log("_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/");
-    console.log("_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/");
-    console.log("\x1b[0m");
-    console.log(request);
+    // return NextResponse.json({
+    //         "type": "rich",
+    //         "version": "1.0",
+    //         "title": "埋め込みどうでしょう",
+    //         "author_name": "t4y3",
+    //         "html": "<div style=\"width:100%;height:120px;border:1px solid;font-weight:bold;overflow:hidden;background-color:white;\"><img src=\"https://t4y3.github.io/oembed-sample/img/Bears.jpg\" width=120 height=120 style=\"float:left;margin-right:10px;\"/>くまさん</div>",
+    //         "height": 500,
+    //         "width": 500,
+    //     }, {status: 200}
+    // )
 
     return NextResponse.json({
-            "type": "rich",
-            "version": "1.0",
-            "title": "埋め込みどうでしょう",
-            "author_name": "t4y3",
-            "html": "<div style=\"width:100%;height:120px;border:1px solid;font-weight:bold;overflow:hidden;background-color:white;\"><img src=\"https://t4y3.github.io/oembed-sample/img/Bears.jpg\" width=120 height=120 style=\"float:left;margin-right:10px;\"/>くまさん</div>",
-            "height": 500,
-            "width": 500,
-        }, {status: 200}
-    )
+        "version": "1.0",
+        "type": "photo",
+        "width": 240,
+        "height": 160,
+        "title": "ZB8T0193",
+        "url": "http://farm4.static.flickr.com/3123/2341623661_7c99f48bbf_m.jpg",
+        "author_name": "Bees",
+        "author_url": "http://www.flickr.com/photos/bees/",
+        "provider_name": "Flickr",
+        "provider_url": "http://www.flickr.com/"
+    })
 }
