14fbd781facf021acfbb84970cd758f1228e520b
t4y3 <y.teraoka8@gmail.com>
2024-06-14 12:40:30 +0900

    fix

--- app/api/oembed_rich/route.js
+++ app/api/oembed_rich/route.js
@@ -1,20 +1,23 @@
-import { NextResponse} from "next/server";
+import {NextResponse} from "next/server";
 
 // < \u003C
 // > \u003E
 
 export async function GET(request) {
     const response = NextResponse.json({
-        "version": "1.0",
-        "author_name": "t4y3",
-        "html": "\u003Ciframe allowfullscreen allow=\"autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture\" src=\"https://oembed-sample.vercel.app/rich/123/embed\" frameborder=\"0\" width=\"300\" height=\"300\" \u003E\u003C/iframe\u003E",
+        "html": "\u003Ciframe allowfullscreen allow=\"autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture\" src=\"https://oembed-sample.vercel.app/rich/123/embed\" frameborder=\"0\" width=\"300\" height=\"300\"\u003E\u003C/iframe\u003E",
+        "iframe_url": "https://oembed-sample.vercel.app/rich/123/embed",
         "width": 300,
         "height": 300,
+        "version": "1.0",
         "provider_name": "oEmbed Sample",
         "provider_url": "https://oembed-sample.vercel.app",
         "type": "rich",
-        "title": "ku"
-    }, { status: 200 });
+        "title": "ku",
+        "thumbnail_url": "http://farm4.static.flickr.com/3123/2341623661_7c99f48bbf_m.jpg",
+        "thumbnail_width": 300,
+        "thumbnail_height": 300
+    }, {status: 200});
 
 
     response.headers.set('Access-Control-Allow-Origin', '*');
