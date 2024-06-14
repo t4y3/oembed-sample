a640e971be7f7df5c1403fbe87f22cd5d4ae17c1
t4y3 <y.teraoka8@gmail.com>
2024-06-14 10:10:40 +0900

    fix

--- app/api/oembed_rich/route.js
+++ app/api/oembed_rich/route.js
@@ -7,7 +7,7 @@ export async function GET(request) {
     const response = NextResponse.json({
         "version": "1.0",
         "author_name": "t4y3",
-        "html": "\u003Ciframe allowfullscreen allow=\"autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture\" src=\"https://oembed-sample.vercel.app/rich/123/embed\" frameborder=\"0\" width=\"300\" height=\"300\" allowfullscreen\u003E\u003C/iframe\u003E",
+        "html": "\u003Ciframe allowfullscreen allow=\"autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture\" src=\"https://oembed-sample.vercel.app/rich/123/embed\" frameborder=\"0\" width=\"300\" height=\"300\" \u003E\u003C/iframe\u003E",
         "width": 300,
         "height": 300,
         "provider_name": "oEmbed Sample",
