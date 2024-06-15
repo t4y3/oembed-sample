6b98aa40004d09c1699730d472e14fced127174a
t4y3 <y.teraoka8@gmail.com>
2024-06-15 22:23:23 +0900

    fix

--- app/blog/[blogId]/page.jsx
+++ app/blog/[blogId]/page.jsx
@@ -1,10 +1,10 @@
 export const runtime = 'edge';
 
-export default function Page() {
+export default function Page({ params }) {
     return (
         <main>
             <h1>ブログ記事ページ</h1>
-            <a href="https://oembed-sample.pages.dev/rich/14543543" className="embedly-card">Embedly</a>
+            <a href={`https://oembed-sample.pages.dev/e/rich/${params.blogId}`} className="embedly-card">Embedly</a>
         </main>
     );
 }
