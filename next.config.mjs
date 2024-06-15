2ff665c3a8266327e2b5d2b59bfbc20d707ad364
t4y3 <y.teraoka8@gmail.com>
2024-06-16 00:27:22 +0900

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "standalone",
+    async rewrites() {
+        return [
+            {
+                "source": "/api/oembed",
+                "destination": "/api/oembed"
+            }
+        ]
+    },
     async headers() {
         return [
             {
