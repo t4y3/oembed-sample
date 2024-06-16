18633be11eab6e374f64e9828fee2b79024991b7
t4y3 <y.teraoka8@gmail.com>
2024-06-16 10:25:52 +0900

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -15,7 +15,7 @@ const nextConfig = {
     },
     async headers() {
         const cspHeader = `
-    default-src 'self';
+    default-src 'self' http://* https://*;
     script-src 'self' 'unsafe-eval' 'unsafe-inline' http://* https://*;
     connect-src 'self' http://* https://*;
     frame-src 'self' http://* https://*;
