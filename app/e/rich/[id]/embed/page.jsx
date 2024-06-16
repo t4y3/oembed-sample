d26b29242a447f604fea67665566e149e1420262
t4y3 <y.teraoka8@gmail.com>
2024-06-16 23:32:09 +0900

    fix

--- app/e/rich/[id]/embed/page.jsx
+++ app/e/rich/[id]/embed/page.jsx
@@ -10,16 +10,16 @@ export default function Page({params}) {
             <h1>Embedされるページです #{params.id}</h1>
             <ul>
                 <li>
-                    <a href="/blog/1">/blog/1へ遷移</a>
+                    <a href="/blog/1" target="_parent">/blog/1へ遷移</a>
                 </li>
                 <li>
-                    <a href="/blog/2">/blog/2へ遷移</a>
+                    <a href="/blog/2" target="_parent">/blog/2へ遷移</a>
                 </li>
                 <li>
-                    <a href="/blog/3">/blog/3へ遷移</a>
+                    <a href="/blog/3" target="_parent">/blog/3へ遷移</a>
                 </li>
                 <li>
-                    <a href="/blog/4">/blog/4へ遷移</a>
+                    <a href="/blog/4" target="_parent">/blog/4へ遷移</a>
                 </li>
             </ul>
         </main>
