7b2e21a8527e25f24ed5473e8fe3fde25c7f67ab
t4y3 <y.teraoka8@gmail.com>
2024-06-16 09:12:50 +0900

    fix

--- app/e/rich/[id]/embed/page.jsx
+++ app/e/rich/[id]/embed/page.jsx
@@ -1,9 +1,11 @@
 export const runtime = 'edge';
 
-export default function Page({ params }) {
+export default function Page({params}) {
     return (
         <main style={{
+            height: '100%',
             backgroundColor: "lightblue",
+            color: "lightblue"
         }}>
             <h1>Embedされるページです #{params.id}</h1>
         </main>
